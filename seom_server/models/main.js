e815147612608f3364883c718c14ca37aa360a0b
Hyeonju <parkhj2416@gmail.com>
2021-08-08 22:34:22 +0900

    [main-issue#7] 지도에 보여줄 정보 불러오기

--- seom_server/models/main.js
+++ seom_server/models/main.js
@@ -2,15 +2,18 @@ const pool = require('../modules/pool');
 
 const main = {
     getMainInfo : async(userIdx) => {
-        const query = `	SELECT ild.deceasedName, ild.deceasedProfileImg
-                        FROM user usr
-                        JOIN user_island usr_ild ON usr.userIdx = usr_ild.userIdx
-                        JOIN island ild ON ild.userIdx = usr.userIdx
-                        WHERE usr.userIdx = ?`;
+        const nameQuery = `SELECT name FROM user WHERE userIdx = ?`;
+        const deceasedQuery = `	SELECT ild.deceasedName, ild.deceasedProfileImg
+                        FROM island ild
+                        WHERE ild.userIdx = ?`;
         
         try {
-            let result = await pool.queryParamArr(query, [userIdx]);
-            
+            let result = {};
+            let nameResult = await pool.queryParamArr(nameQuery, [userIdx]);
+            let deceasedResult = await pool.queryParamArr(deceasedQuery, [userIdx]);
+
+            result.name = nameResult[0].name;
+            result.deceasedInfo = deceasedResult;
             return result;
         } catch(err) {
             console.log('getMainInfo err: ', err);
